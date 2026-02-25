3958290646a79b899ccb04bc59a44aaa42b9a037
Ricardo031 <rjtb31@gmail.com>
2026-02-25 16:27:53 -0500

    Feat: plus and subtract

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-print("Bienvenido Calculadora.py")
-opciones = int(input("Menu de opciones: "))
+import sys
+print("Welcome to calculator.py")
 
-while opciones != 4:
-    opciones =["Suma(0)","Resta(1)","Multiplicacion(2)","Division(3)","Exit(4)",]
+options=["Plus (0)","Subtract (1)","Multiplication (2)","Division (3)","Exit (4)"]
+for op in options:
+    print(op)
+try:
+    option = int(input("Choose any operation: "))
+    if option== 4:
+        print("Closing program..")
+        sys.exit()
+    num1= float(input("Type any number: "))
+    num2= float(input("Type another number: "))
+    if option== 0:
+        print(f"Plus: {num1 + num2}")
+    elif option== 1:
+        print(f"Subtract: {num1 - num2}")
+except:
+    print("Bye")
