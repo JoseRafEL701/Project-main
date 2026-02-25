dd080f1de7fefd9175f94b2699c722f66f252e44
lilimuoz <l-1998@hotmail.com>
2026-02-25 10:13:53 -0500

    "whole code"

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
-print("Bienvenido Calculadora.py")
-opciones = int(input("Menu de opciones: "))
-
-while opciones != 4:
-    opciones =["Suma(0)","Resta(1)","Multiplicacion(2)","Division(3)","Exit(4)",]
+import sys
+print("Welcome to calculator.py")
+options=["Plus (0)","Subtract (1)","Multiplication (2)","Division (3)","Exit (4)"]
+for op in options:
+    print(op)
+try:
+    option = int(input("Choose any operation: "))
+    if option== 4:
+        print("Closing program..")
+        sys.exit()
+    num1= float(input("Type any number: "))
+    num2= float(input("Type another number: "))
+    if option== 0:
+        print(f"Plus: {num1 + num2}")
+    elif option== 1:
+        print(f"Subtract: {num1 - num2}")
+    elif option== 2:
+        print(f"Multiplication: {num1 * num2}")
+    elif option== 3:
+        print(f"Division: {num1 / num2}")
+    else:
+        print("Invalid action!")
+except ZeroDivisionError:
+    print("Cant handle zero division")
+except:
+    print("Bye")
