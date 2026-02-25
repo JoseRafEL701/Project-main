9c06f20d0eb9761cae3d6f99e044e484f7796072
maracuya-acida <aurivaldes11@gmail.com>
2026-02-25 09:07:54 -0500

    feat: se creo el menu

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-numer = 1
+print("Bienvenido Calculadora.py")
+opciones = int(input("Menu de opciones: "))
+
+while opciones != 4:
+    opciones =["Suma(0)","Resta(1)","Multiplicacion(2)","Division(3)","Exit(4)",]
