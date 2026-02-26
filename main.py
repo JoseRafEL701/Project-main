712f1343084d7ac18d4ba25f9e1aa54f9c5dce62
ashley barrios <ashleyrbarriosd@gmail.com>
2026-02-26 06:23:35 -0500

    fix: se corrigio

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 print("Welcome to calculator.py")
 
-options=["Plus (0)","Subtract (1)","Multiplication (2)","Division (3)","Exit (4)"]
+options=["Plus (0)","Subtract (1)","Exit (4)"]
 for op in options:
     print(op)
 try:
