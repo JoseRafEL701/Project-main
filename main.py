a321864f81511f5fa49494d629461da8f4f4f1b7
ashley barrios <ashleyrbarriosd@gmail.com>
2026-02-26 06:27:44 -0500

    se corrigio

--- main.py
+++ main.py
@@ -4,16 +4,3 @@ print("Welcome to calculator.py")
 options=["Plus (0)","Subtract (1)","Multiplication (2)","Division (3)","Exit (4)"]
 for op in options:
     print(op)
-try:
-    option = int(input("Choose any operation: "))
-    if option== 4:
-        print("Closing program..")
-        sys.exit()
-    num1= float(input("Type any number: "))
-    num2= float(input("Type another number: "))
-    if option== 0:
-        print(f"Plus: {num1 + num2}")
-    elif option== 1:
-        print(f"Subtract: {num1 - num2}")
-except:
-    print("Bye")
