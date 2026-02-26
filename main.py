9329a2904d32c2509107e5dfe8bb96e50b654b9c
Ricardo31 <rjtb31@gmail.com>
2026-02-26 06:51:36 -0500

    fix: eliminar lo innecesario

--- main.py
+++ main.py
@@ -10,22 +10,6 @@ while True:
         if option == 4:
             print("Closing program..")
             break
-        num1 = float(input("Type any number: "))
-        num2 = float(input("Type another number: "))
-        if option == 0:
-            print(f"Plus: {num1 + num2}")
-            print("---------------------")
-        elif option == 1:
-            print(f"Subtract: {num1 - num2}")
-            print("---------------------")
-        elif option == 2:
-            print(f"Multiplication: {num1 * num2}")
-            print("---------------------")
-        elif option == 3:
-            print(f"Division: {num1 / num2}")
-            print("---------------------")
-        else:
-            print("Invalid action!")
     except ZeroDivisionError:
         print("Cant handle zero division")
     except:
